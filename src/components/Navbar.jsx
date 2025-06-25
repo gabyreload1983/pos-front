7cbd20b5fe2d6ae4b91cbe748a31c6144c7d250d
Gabriel Godoy <gabyreload@gmail.com>
2025-06-24 23:52:13 -0300

    styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,34 @@ import { Link } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
-  const { isAuthenticated, logout, user } = useAuth();
+  const { isAuthenticated, logout } = useAuth();
 
   return (
-    <nav className="bg-gray-800 p-4 text-white flex gap-4 justify-between">
+    <nav className="bg-white border-b border-gray-200 p-4 text-gray-800 shadow flex justify-between items-center">
       <div className="flex gap-4">
-        <Link to="/">Inicio</Link>
-        {isAuthenticated && <Link to="/ventas">Ventas</Link>}
-        {isAuthenticated && <Link to="/clientes">Clientes</Link>}
+        <Link to="/" className="hover:text-blue-600">
+          Inicio
+        </Link>
+        {isAuthenticated && (
+          <>
+            <Link to="/ventas" className="hover:text-blue-600">
+              Ventas
+            </Link>
+            <Link to="/clientes" className="hover:text-blue-600">
+              Clientes
+            </Link>
+          </>
+        )}
       </div>
       <div>
         {isAuthenticated ? (
-          <button onClick={logout} className="hover:underline">
+          <button onClick={logout} className="hover:underline text-sm">
             Cerrar sesión
           </button>
         ) : (
-          <Link to="/login">Login</Link>
+          <Link to="/login" className="hover:text-blue-600">
+            Login
+          </Link>
         )}
       </div>
     </nav>
